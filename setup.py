48c7c5fc22b33ebd16a78f31794238802a916adf
Abraham Escalante <aeklant@gmail.com>
2020-04-11 12:56:10 -0400

    DEV: added bare-bones setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='cross_entropy_method',
+    version='0.0.0',
+    py_modules=['cross_entropy'],
+
+    # TODO: fill in the required packages
+    install_requires=[
+    ]
+
+    # TODO: add entry points
+)

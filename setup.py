12e8cb5720f3c04806600bc1fefbdae571041264
Abraham Escalante <aeklant@gmail.com>
2020-04-12 14:31:14 -0400

    DOC: updated package version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 
 setup(
     name='cross_entropy_method',
-    version='0.0.0',
+    version='0.0.2',
     py_modules=['cross_entropy'],
 
     # TODO: fill in the required packages
     install_requires=[
+        'numpy',
         'torch',
     ]
 
